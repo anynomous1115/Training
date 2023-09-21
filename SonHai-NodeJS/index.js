006946082c0ccd743f67176d4c4496f6394bb5b0
anynomous1115 <caosonhai1410@gmail.com>
2023-09-21 11:27:34 +0700

    done Json

--- SonHai-NodeJS/index.js
+++ SonHai-NodeJS/index.js
@@ -2,14 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3030;
 const path = require('path')
-const fs = require('fs')
+const fs = require('fs').promises;
 const db = require('./db/db.json');
+const carts = require('./db/carts.json')
 
 app.use(express.urlencoded())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "public")));
 
-// route
+
+const readFileJson = (pathFile) => {
+    return fs.readFile(pathFile, 'utf8', (err, contents) => {
+        resolve(contents)
+    })
+}
+const writeFileJson = (pathFile, data) => {
+    return fs.writeFile(pathFile, data)
+}
+app.post("/test", async (req, res) => {
+    const dataInJson = await readFileJson(__dirname + "/db/carts.json");
+    const dataParse = JSON.parse(dataInJson)
+    dataParse.carts.push({ "id": "10", "quantity": 1 })
+    console.log(dataParse);
+    res.end(fs.writeFile(__dirname + "/db/carts.json", JSON.stringify(dataParse)))
+})
+// route 
 app.get("/", (req, res) =>
     res.render(__dirname + "/public/index.html")
 );
@@ -18,30 +35,28 @@ app.get("/data", (req, res) =>
     res.sendFile(__dirname + "/db/db.json")
 );
 
-app.post("/carts", (req, res) => {
-    fs.readFile(__dirname + "/db/db.json", 'utf8', (err, data) => {
-        data = JSON.parse(data);
-        data.carts.push(req.body)
-        res.end(fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(data)))
-    })
+app.post("/carts", async (req, res) => {
+    const dataInJson = await readFileJson(__dirname + "/db/db.json");
+    const dataParse = JSON.parse(dataInJson)
+    dataParse.carts.push(req.body)
+    res.send(writeFileJson(__dirname + "/db/db.json", JSON.stringify(dataParse)))
 })
 
-app.delete("/carts/:id", (req, res) => {
-    fs.readFile(__dirname + "/db/db.json", 'utf8', (err, data) => {
-        data = JSON.parse(data);
-        const index = data.carts.findIndex(i => i.id == req.params.id)
-        data.carts.splice(index, 1)
-        res.end(fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(data)))
-    })
+app.delete("/carts/:id", async (req, res) => {
+    const dataInJson = await readFileJson(__dirname + "/db/db.json");
+    const dataParse = JSON.parse(dataInJson)
+    const index = dataParse.carts.findIndex(i => i.id == req.params.id)
+    dataParse.carts.splice(index, 1)
+    res.send(writeFileJson(__dirname + "/db/db.json", JSON.stringify(dataParse)))
 })
 
-app.put("/carts/:id", (req, res) => {
-    fs.readFile(__dirname + "/db/db.json", 'utf8', (err, data) => {
-        data = JSON.parse(data);
-        const index = data.carts.findIndex(i => i.id == req.params.id)
-        data.carts[index].quantity = req.body.quantity
-        res.end(fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(data)))
-    })
+app.put("/carts/:id", async (req, res) => {
+    const dataInJson = await readFileJson(__dirname + "/db/db.json");
+    const dataParse = JSON.parse(dataInJson)
+    const index = dataParse.carts.findIndex(i => i.id == req.params.id)
+    dataParse.carts[index].quantity = req.body.quantity
+    res.send(writeFileJson(__dirname + "/db/db.json", JSON.stringify(dataParse)))
+
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
--- SonHai-NodeJS/public/test/test.js
+++ SonHai-NodeJS/public/test/test.js
@@ -1,47 +1,12 @@
-// let cart = []
-// const get = async () => {
-//     await fetch(`http://localhost:3000/carts`)
-//         .then(res => res.json())
-//         .then(data => cart = data)
-
-//         .catch((error) => {
-//             console.log(error);
-//         })
-// }
-// const main = async () => {
-//     await get()
-//     console.log(cart);
-// }
-// main()
-// const promise = new Promise((resolve, reject) => {
-//     resolve()
-// })
-// promise.then((result) => {
-//     return result + 1
-// })
-//     .then((result) => {
-//         return result + 1
-//     })
-//     .then((result) => {
-//         console.log(result);
-//     })
-
-const arr = [
-    {
-        id: 11,
-        quantity: 1
-    },
-    {
-        id: 11,
-        quantity: 8
-    },
-    {
-        id: 11,
-        quantity: 9
-    },
-
-]
-const someArr = arr.some((element) => {
-    return element.quantity > 10
-})
-console.log(someArr);
+const { readFile } = require('node:fs/promises');
+const { resolve } = require('node:path');
+async function logFile() {
+  try {
+    const filePath = resolve('./package.json');
+    const contents = await readFile(filePath, { encoding: 'utf8' });
+    console.log(contents);
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+logFile();
