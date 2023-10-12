75096a1e026d10315b2f318ecb3ca98fdda1a171
anynomous1115 <caosonhai1410@gmail.com>
2023-10-12 09:26:03 +0700

    done

--- SonHai-NodeJS/src/middlewares/checkEmailAndPass.js
+++ SonHai-NodeJS/src/middlewares/checkEmailAndPass.js
@@ -1,58 +1,18 @@
-const checkEmail = (req, res, next) => {
-  const { email } = req.body;
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const isValidEmail = emailRegex.test(email);
-
-  if (!isValidEmail) {
-    res.status(400).json({ message: "Invalid email" });
-    return;
-  }
-
-  next();
-};
-const checkPassword = (req, res, next) => {
-  const { password, rePassword } = req.body;
-
-  if (rePassword !== password) {
-    res.status(400).json({ message: "Confirm Password not match" });
-    return;
-  }
-
-  if (password.length < 8) {
-    res
-      .status(400)
-      .json({ message: "Password must be greater than 8 characters" });
-    return;
-  }
-
-  const passwordRegex =
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&+=])[A-Za-z\d@#$%^&+=]{8,}$/;
-  const isValidPassword = passwordRegex.test(password);
-
-  if (!isValidPassword) {
-    res.status(400).json({
-      message: "Password must contain letters, numbers, and special characters",
-    });
-    return;
-  }
-  next();
-};
-
 const validBodyData = (schema, fieldCheck) => (req, res, next) => {
   const body = req.body;
-  let hasError = false
-  
+  let hasError = false;
+
   fieldCheck.forEach((element) => {
     if (typeof body[element] !== typeof schema[element].type) {
       res.status(400).json({ message: `Invalid ${element}` });
-      hasError = true
+      hasError = true;
       return;
     }
 
     const isValid = schema[element].regex.test(body[element]);
     if (!isValid) {
       res.status(400).json({ message: `Invalid ${element}` });
-      hasError = true
+      hasError = true;
       return;
     }
   });
@@ -62,7 +22,5 @@ const validBodyData = (schema, fieldCheck) => (req, res, next) => {
 };
 
 module.exports = {
-  checkEmail,
-  checkPassword,
   validBodyData,
 };
